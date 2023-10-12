01691413087cee619fec29c7263cfeb7c5c27806
mainangaruiya <003paulmaina@gmail.com>
2023-10-12 10:16:24 +0300

    SQL script that creates a table users following these requirements

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- creates a table users with follwing fields
 -- id, email, name
 CREATE TABLE IF NOT EXISTS users (
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT'
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
