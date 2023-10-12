7585e4acba1a1bbc851ba611596a7239010bb014
mainangaruiya <003paulmaina@gmail.com>
2023-10-12 10:32:45 +0300

    add country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS users(
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
