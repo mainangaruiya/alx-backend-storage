bccf628a2cdf73be5fd4fb88121d0d121de5dc15
mainangaruiya <003paulmaina@gmail.com>
2023-10-12 10:30:24 +0300

    add country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
---create table users with
---id, email, name ,country
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+-- Script that creates a table users following these requirements:
+-- id, email, name, country(enumeration of US, CO and TN)
+CREATE TABLE IF NOT EXISTS users(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
